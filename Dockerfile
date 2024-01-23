ac05f7826b247a285a12199afcd65b6f34e9b7c4
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2024-01-23 22:24:44 +0800

    finalizing #3

--- Dockerfile
+++ Dockerfile
@@ -27,7 +27,7 @@ WORKDIR /app
 COPY --from=deps /app/node_modules ./node_modules
 COPY . .
 
-# ENV NEXT_TELEMETRY_DISABLED 1
+ENV NEXT_TELEMETRY_DISABLED 1
 
 RUN \
     if [ -f yarn.lock ]; then SKIP_ENV_VALIDATION=1 yarn build; \
@@ -43,7 +43,7 @@ WORKDIR /app
 
 ENV NODE_ENV production
 
-# ENV NEXT_TELEMETRY_DISABLED 1
+ENV NEXT_TELEMETRY_DISABLED 1
 
 COPY --from=builder /app/next.config.js ./
 COPY --from=builder /app/public ./public
--- sst.config.ts
+++ sst.config.ts
@@ -1,22 +1,16 @@
-import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+import { type SSTConfig } from "sst";
 import { Service } from "sst/constructs";
 
 export default {
   config(_input) {
     return {
       name: "t3-sst-docker",
-      region: "us-east-1",
+      region: "ap-southeast-1",
     };
   },
   stacks(app) {
+    app.setDefaultRemovalPolicy("destroy");
     app.stack(function Site({ stack }) {
-      // const site = new NextjsSite(stack, "site");
-
-      // stack.addOutputs({
-      //   SiteUrl: site.url,
-      // });
-
       const nextjsService = new Service(stack, "nextjs", {
         path: ".",
         port: 3000,
